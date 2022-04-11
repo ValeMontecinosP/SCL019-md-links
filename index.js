6afadbd1e19d701f31c5c97c5eed308d08d38e89
ValeMontecinosP <valemontecinosp@gmail.com>
2022-04-11 13:38:50 -0400

    avance

--- index.js
+++ index.js
@@ -1,28 +1,72 @@
 // // module.exports = () => {
 // //   // ...
 // // };
-// //esta es una prueba
 const fs = require("fs");
-// const {readFile} = require("fs");
 const path = require('path');
 const colors = require("colors");
-
+// const MarkdownIt = require ("markdown-it"),
+//   md = new MarkdownIt();
+// const result = md.render("# markdownt-it rulez")
+// console.log(result)
 const prompt = require ( 'prompt-sync' ) ( ) ; 
 let promptValue = prompt('Ingresa la ruta del archivo: ');
 console.log(`ruta: ${promptValue}`);
 
-// // fs.readFile("./README.md", "utf-8", (error, archive)=> {
-// //   if (error) throw error;
-// //   console.log(archive.red);
-// // });
+const md = require('markdown-it')();
+console.log(`${(md.validateLink("hola"))}`.red)
 
-// console.log(process.argv)
-// console.log("hola".blue)
+function isAbsolutePath (promptValue) {
+  if (path.isAbsolute(promptValue) == false) {
+    console.log(`Ruta absoluta: ${path.resolve(promptValue)}`.bgGreen);
+  };
+}
 
-console.log(`Es o no absoluta: ${path.isAbsolute(promptValue)}`.bgYellow);
-console.log(`Extension: ${path.extname(promptValue)}`.rainbow);
-console.log(`Ruta absoluta: ${path.resolve(promptValue)}`.bgGreen);
-console.log(__dirname)
+// const validLink = (input) => {
+//   const md = require('markdown-it')();
+//   // enable everything
+//   md.validateLink(input)
+  
+//   console.log(`¿es un enlace válido? ${md.validateLink()}`)
+// };
+
+const mdData = () => fs.readFile(promptValue, "utf-8", (error, archive)=> {
+  if (error){
+    console.log("archivo no existe");
+  }
+  else{
+    console.log(archive); //esto se puede borrar después
+  }
+  return archive
+});
+
+function validMd(promptValue) {
+  if (path.extname(promptValue)==".md") {
+    isAbsolutePath(promptValue);
+    mdData();
+    
+  }
+  else {
+    console.log(`chao esto no es .md, es Extension: ${path.extname(promptValue)}`.rainbow)
+  }
+};
+validMd(promptValue);
+//validLink(mdData);
+
+// if (validLink=="true"){
+//   return console.log(`${mdData} es un link válido`)
+// } else {
+//   return console.log(`${mdData} no es un link válido`)
+// }
+
+
+
+//console.log(`Es o no absoluta: ${path.isAbsolute(promptValue)}`.bgYellow);
+
+//console.log(__dirname)
+// console.log(fs.readdir(promptValue, (error, archive)=> {
+//   if (error) throw error;
+//   console.log(archive.red);
+// }))
 
 // const mdlinks = require('dc-md-links');
 
